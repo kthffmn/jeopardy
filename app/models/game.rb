4624908703b4a24ca6f3e64a039856ba55c57ad1
Katie Hoffman <ktahoffman@gmail.com>
2014-12-18 15:11:42 -0500

    add method #find_questions which returns a hash of keys, categories, and values, array of questions

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,11 +1,21 @@
 class Game < ActiveRecord::Base
   belongs_to :show
+  has_many :questions, through: :show
+  has_many :prizes, -> { uniq }, through: :show
+  has_many :rounds, -> { uniq }, through: :show
+  has_many :categories, -> { uniq }, through: :show
+
   has_many :user_games
   has_many :users, through: :user_games
 
   def find_questions(round_name)
     jeopardy = Round.find_by(:name => round_name)
-    self.show.questions.where(:round_id => jeopardy.id)
+    qs = questions.where(:round_id => jeopardy.id)
+    qs.each_with_object({}) do |q,h|
+      key = q.category.name
+      h[key] = [] unless h[key]
+      h[key] << q
+    end
   end
 
   def jeopardy
